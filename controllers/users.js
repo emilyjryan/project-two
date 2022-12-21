00482d331789341f90348537147ed019534438b5
emilyjryan <emilyryanmac@gmail.com>
2022-12-21 15:53:28 -0700

    created profile view and link

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,9 @@ const router = express.Router()
 
 // GET /users/new -- serves a form to create a new user
 router.get('/new', (req,res) => {
-    res.render('./users/new.ejs')
+    res.render('./users/new.ejs', {
+        user: res.locals.user
+    })
 })
 
 // POST /users -- creates a new user from the form @ /users/new
@@ -38,7 +40,8 @@ router.post('/', async (req,res) => {
 // GET /users/login -- render a login form that posts to /users/login
 router.get('/login', (req,res) => {
     res.render('users/login.ejs', {
-        message: req.query.message ? req.query.message : null
+        message: req.query.message ? req.query.message : null,
+        user: res.locals.user
     })
 })
 
@@ -64,7 +67,7 @@ router.post('/login', async (req,res) => {
             // if the user is found and their PW matches, log them in
             console.log('logging user in') 
             res.cookie('userId', user.id)
-            res.redirect('/')
+            res.redirect('/users/profile')
         }
         
     } catch (err) {
@@ -79,6 +82,18 @@ router.get('/logout', (req,res) => {
     //make a get req to /
     res.clearCookie('userId')
     res.redirect('/')
+})
+
+// GET /users/profile -- show the user their profile page
+router.get('/profile', (req,res) => {
+    // if the user is not logged in then they are not allowed to be here
+    if (!res.locals.user) {
+        res.redirect('/users/login?message=You must authenticate before you are authorized to view this resource!')
+    } else {
+        res.render('users/profile.ejs', {
+            user: res.locals.user
+        })
+    }
 })
 
 // export the router
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // required packages
 require('dotenv').config()
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const db = require('./models')
 
 // app config
 const app = express()
@@ -9,10 +11,46 @@ app.set('view engine', 'ejs')
 
 // parse request bodies from html forms
 app.use(express.urlencoded({ extended: false }))
+// tell express to parse incoming cookies
+app.use(cookieParser())
+
+// our goal is to have a custom auth middleware that checks the cookies for a user id and if it finds one, we look up the user in the db
+// then tell all downstream routes about this user
+app.use(async (req,res,next) => {
+try {
+    if (req.cookies.userId) {
+        // the user is logged in, let's find them in the db
+        const user = await db.user.findByPk(req.cookies.userId)
+        // mount the logged in user on the res.locals
+        res.locals.user = user
+    } else {
+        res.locals.user = null
+    }
+    //move on to next middleware route
+    next()
+} catch (err) {
+    console.log('error in auth middleware 🔥🔥🔥🔥🔥')
+    next() //go to the next thing
+}
+})
+
+//example custom middleware (incoming request logger)
+app.use((req,res,next) => {
+    //our code goes here
+    // console.log('hello from inside of the middleware')
+    console.log(`incoming request: ${req.method} - ${req.url}`)
+    // res.locals are a place we can put data to share with downstream routes
+    // res.locals.myData = 'hello I am data'
+    // invoke next to tell express to go to the next route or middleware
+    next()
+})
 
 // routes and controllers
 app.get('/', (req,res) => {
-    res.render('home.ejs')
+    console.log(res.locals.user)
+    res.render('home.ejs', {
+        user: res.locals.user
+    })
 })
 
 // controllers
